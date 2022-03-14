9bf314fdfc1c1af18a30764b50d393b3aaf39880
rchou <1272970733@qq.com>
2022-03-14 16:44:04 +0800

    Initial commit

---
+++ SpringbootSchoolShop/src/main/java/com/zhang/ssmschoolshop/config/WebMvcConfig.java
@@ -0,0 +1,35 @@
+package com.zhang.ssmschoolshop.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+/**
+ * @author created by CodingZhangxin
+ * @version v.0.1
+ * @Description TODO
+ * @备注  springboot内置tomcat配置虚拟路径
+ *      linux： /usr/upload  /pictures
+ *      window:  d:/upload  /pictures
+ **/
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String os = System.getProperty("os.name");
+        String pathPatterns="/pictures/**";
+        String pathAbsolute="file:D:/upload/";
+        if (os.toLowerCase().startsWith("linux")){
+            pathAbsolute="file:/usr/upload/";
+        }
+        registry.addResourceHandler(pathPatterns).addResourceLocations(pathAbsolute);
+
+    }
+
+}
